10948b9ed79b117b4a7d6b085fc2b183411ed19a
wanggang <arthurwanggang@yeah.net>
2021-09-18 23:19:34 +0800

    1：整理auth0和jjwt两种方式实现基本权限的控制

---
+++ missyou/src/main/java/com/lin/missyou/auth/Auth0.java
@@ -0,0 +1,6 @@
+package com.lin.missyou.auth;
+
+
+// 关于Java Auth0的基本概念和实现
+public class Auth0 {
+}
---
+++ missyou/src/main/java/com/lin/missyou/auth/JavaJWT.java
@@ -0,0 +1,7 @@
+package com.lin.missyou.auth;
+
+
+// 关于jjjwt的基本概念和基本权限实现
+public class JavaJWT {
+
+}
