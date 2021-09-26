273d05975e5a0009c64c36b3ba04f9c4e238654b
wanggang <arthurwanggang@yeah.net>
2021-09-26 23:49:05 +0800

    1：增加不同种类的优惠券查询接口

--- missyou/src/main/java/com/lin/missyou/repository/CouponRepository.java
+++ missyou/src/main/java/com/lin/missyou/repository/CouponRepository.java
@@ -3,9 +3,11 @@ package com.lin.missyou.repository;
 import com.lin.missyou.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
@@ -17,5 +19,54 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
             "and a.startTime < :now\n" +
             "and a.endTime > :now\n")
     List<Coupon> findByCategory(Long cid, Date now);
+    @Query("select c from Coupon c\n" +
+            "join Activity a on c.activityId = a.id\n" +
+            "where c.wholeStore = :isWholeStore\n" +
+            "and a.startTime < :now\n" +
+            "and a.endTime > :now\n" )
+
+    List<Coupon> findByWholeStore(Boolean isWholeStore, Date now);
+
+
+    @Query("select c from Coupon c\n" +
+            "join UserCoupon uc\n" +
+            "on c.id = uc.couponId\n" +
+            "join User u\n" +
+            "on u.id = uc.userId\n" +
+            "where uc.status = 1 \n" +
+            "and u.id = :uid\n" +
+            "and c.startTime < :now\n" +
+            "and c.endTime > :now\n" +
+            "and uc.orderId is null")
+    List<Coupon> findMyAvailable(Long uid, Date now);
+
+
+
+    @Query("select c From Coupon c\n" +
+            "join UserCoupon uc\n" +
+            "on c.id = uc.couponId\n" +
+            "join User u\n" +
+            "on u.id = uc.userId\n" +
+            "where u.id = :uid\n" +
+            "and uc.status = 2\n" +
+            "and uc.orderId is not null \n" +
+            "and c.startTime < :now\n" +
+            "and c.endTime > :now")
+    List<Coupon> findMyUsed(@Param("uid") Long uid, @Param("now") Date now);
+
+
+    @Query("select c From Coupon c\n" +
+            "join UserCoupon uc\n" +
+            "on c.id = uc.couponId\n" +
+            "join User u\n" +
+            "on u.id = uc.userId\n" +
+            "where u.id = :uid\n" +
+            "and uc.orderId is null\n" +
+            "and uc.status <> 2\n" +
+            "and c.endTime < :now")
+    List<Coupon> findMyExpired(Long uid, Date now);
+
+
+    Optional<Coupon> findByIdAndCreateTimeLessThanAndEndTimeGreaterThan(Long id, Date now1, Date now2);
 
 }
