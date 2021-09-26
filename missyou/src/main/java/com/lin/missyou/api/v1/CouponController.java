5383e6bd7479daa4b44b30cb2137a91853066933
wanggang <arthurwanggang@yeah.net>
2021-09-26 23:57:55 +0800

    1：增加不同种类的优惠券查询接口Controllers

--- missyou/src/main/java/com/lin/missyou/api/v1/CouponController.java
+++ missyou/src/main/java/com/lin/missyou/api/v1/CouponController.java
@@ -1,7 +1,10 @@
 package com.lin.missyou.api.v1;
 
 
+import com.lin.missyou.core.UnifyResponse;
+import com.lin.missyou.core.interceptors.ScopeLevel;
 import com.lin.missyou.model.Coupon;
+import com.lin.missyou.model.User;
 import com.lin.missyou.service.CouponService;
 import com.lin.missyou.vo.CouponPureVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @ResponseBody
@@ -29,5 +33,58 @@ public class CouponController {
             return Collections.emptyList();
         }
         return CouponPureVO.getList(couponList);
+    }
+    @GetMapping("/whole_store")
+    public List<CouponPureVO> getWholeStoreCouponList(@Autowired CouponService couponService) {
+        List<Coupon> coupons = this.couponService.getWholeStoreCoupons();
+        if (coupons.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return CouponPureVO.getList(coupons);
+    }
+
+    @ScopeLevel()
+    @PostMapping("/collect/{id}")
+    public void collectCoupon(@PathVariable Long id) {
+        Long uid = LocalUser.getUser().getId();
+        couponService.collectOneCoupon(uid, id);
+        UnifyResponse.createSuccess(0);
+    }
+
+    @ScopeLevel
+    @GetMapping("/myself/by/status/{status}")
+    public List<CouponPureVO> getMyCouponByStatus(@PathVariable Integer status) {
+        Long uid = LocalUser.getUser().getId();
+        List<Coupon> couponList;
+
+        //触发机制 时机 过期
+        switch (CouponStatus.toType(status)) {
+            case AVAILABLE:
+                couponList = couponService.getMyAvailableCoupons(uid);
+                break;
+            case USED:
+                couponList = couponService.getMyUsedCoupons(uid);
+                break;
+            case EXPIRED:
+                couponList = couponService.getMyExpiredCoupons(uid);
+                break;
+            default:
+                throw new ParameterException(40001);
+        }
+        return CouponPureVO.getList(couponList);
+    }
+
+    @ScopeLevel()
+    @GetMapping("/myself/available/with_category")
+    public List<CouponCategoryVO> getUserCouponWithCategory() {
+        User user = LocalUser.getUser();
+        List<Coupon> coupons = couponService.getMyAvailableCoupons(user.getId());
+        if (coupons.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return coupons.stream().map(coupon -> {
+            CouponCategoryVO vo = new CouponCategoryVO(coupon);
+            return vo;
+        }).collect(Collectors.toList());
     }
 }
