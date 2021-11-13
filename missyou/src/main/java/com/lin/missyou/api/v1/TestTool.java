3dd177ea41555225b5135c899af00d46103d5d27
wanggang <arthurwanggang@yeah.net>
2021-11-13 23:57:59 +0800

    1: javac成功链接es和相关配置

--- missyou/src/main/java/com/lin/missyou/api/v1/TestTool.java
+++ missyou/src/main/java/com/lin/missyou/api/v1/TestTool.java
@@ -1,20 +1,21 @@
 package com.lin.missyou.api.v1;
-
+import co.elastic.clients.elasticsearch._core.IndexRequest;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.lin.missyou.util.ImportJsonToElasticsearch;
-import org.apache.logging.log4j.util.StringMap;
-import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.plugin2.message.transport.Transport;
-
-import javax.management.monitor.StringMonitor;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,7 +32,8 @@ public class TestTool {
     }
 
     @SuppressWarnings("unchecked")
-    public List<Object> convertStringToList(String jsonString) {
+    public List<Object> convertStringToList(String jsonString) throws IOException {
+        createIndex();
         return (List<Object>) JSON.parse(jsonString);
     }
 
@@ -42,9 +44,28 @@ public class TestTool {
         return stringMap;
     }
 
-    public void importToElasticsearch(){
-        // 链接es
-        
+    public RestHighLevelClient getClient() {
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost("127.0.0.1", 9200, "http")));
     }
 
+    // 创建索引
+    public void createIndex() throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest("posts");
+        request.settings(Settings.builder()
+                .put("index.number_of_shards", 3)
+                .put("index.number_of_replicas", 2)
+        );
+        request.mapping(
+                "{\n" +
+                        "  \"properties\": {\n" +
+                        "    \"message\": {\n" +
+                        "      \"type\": \"text\"\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}",
+                XContentType.JSON);
+        CreateIndexResponse createIndexResponse = this.getClient().indices().create(request, RequestOptions.DEFAULT);
+        System.out.println("createIndexResponse" + createIndexResponse);
+    }
 }
